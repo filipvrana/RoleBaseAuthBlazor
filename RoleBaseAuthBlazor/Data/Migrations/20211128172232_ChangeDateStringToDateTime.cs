bf0c6d019c0594e5727499dc66f41200f2ff2987
Filip Vrána <filosek.vrana@gmail.com>
2021-11-28 18:26:26 +0100

    28.11.2021-funkční

---
+++ RoleBaseAuthBlazor/Data/Migrations/20211128172232_ChangeDateStringToDateTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoleBaseAuthBlazor.Data.Migrations
+{
+    public partial class ChangeDateStringToDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Length",
+                table: "Practices",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Practices",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Length",
+                table: "Practices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "Practices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
--- RoleBaseAuthBlazor/Data/Practice.cs
+++ RoleBaseAuthBlazor/Data/Practice.cs
@@ -3,8 +3,8 @@
     public class Practice
     {
         public int Id { get; set; }
-        public string Date { get; set; }
-        public string Length { get; set; }
+        public DateTime? Date { get; set; }
+        public int Length { get; set; }
         public string Location { get; set; }
     }
 }
